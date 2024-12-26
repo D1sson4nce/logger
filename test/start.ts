5ced1a9fec5c609fa1b352d0083c12fc08933b31
D1sson4nce <45755310+D1sson4nce@users.noreply.github.com>
2024-12-26 02:45:43 +0100

    gone test

--- test/start.ts
+++
@@ -1,8 +0,0 @@
-import { Logger } from "..";
-import { Test } from "./test";
-
-Logger.configure({
-})
-
-new Test().test("sebber", 23, { fornite: "battlepass" })
-new Test().test("sebber", 42)
--- test/test.ts
+++
@@ -1,9 +0,0 @@
-import { Log } from "..";
-
-export class Test {
-    @Log(["fornite"])
-    test(name: string, age: number, obj?: any) {
-        return `${name}: ${age}`
-    }
-}
-
